7a1a25cdb407c910c6d0a429692a19f1f947e3ad
zaid251 <zaidahmededu@gmail.com>
2026-04-22 15:44:01 +0530

    Integrated React Router for multi-page support

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import StudyGuide from './StudyGuide.jsx'
+import Paper2024 from './2024Paper.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         {/* Your new study guide is at: /guide */}
         <Route path="/guide" element={<StudyGuide />} />
+        <Route path="/guide-2" element={<Paper2024 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
